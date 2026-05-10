eea54fafd600bbd8e5684bcf0bb12c439a0aa23a
carlosaprost <carlosaprost@gmail.com>
2026-05-10 01:05:44 -0300

    chore: release 3.0.4 (Production Config)

--- src/app/components/about-dialog/about-dialog.ts
+++ src/app/components/about-dialog/about-dialog.ts
@@ -18,7 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
       <div class="about-header text-center">
         <img src="assets/icon.png" alt="SERA Logo" class="about-logo">
         <h1 class="about-title">SERA</h1>
-        <p class="about-version">Versión 3.0.3 "Orion"</p>
+        <p class="about-version">Versión 3.0.4 "Orion"</p>
       </div>
       
       <div class="about-content">
